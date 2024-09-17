eb5667e710fae3520a5f46150dd625c04272afd9
Agus026h <agustin201h@gmail.com>
2024-09-17 19:00:24 -0300

    clase producto

---
+++ src/gp9tp6/Producto.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gp9tp6;
+
+/**
+ *
+ * @author agust
+ */
+public class Producto {
+    private String nombre;
+    private String categoria;
+    private double precio;
+
+    public Producto(String nombre, String categoria, double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "Nombre " + nombre + ", Categoria " + categoria + ", Precio " + precio + '}';
+    }
+    
+    
+    
+    
+}
